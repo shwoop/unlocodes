d214703c5aa2cb3dc606741afd6171dfd9f930d5
Alistair Ferguson <alistair.ferguson@polestarglobal.com>
2019-08-13 16:50:59 +0100

    Polish the data

--- unlocodes.py
+++ unlocodes.py
@@ -1,9 +1,9 @@
-#!/usr/bin/python3
 import argparse
 import glob
 import os
 
 import IPython
+import numpy as np
 import pandas as pd
 
 
@@ -57,46 +57,120 @@ def load_unlocodes() -> pd.DataFrame:
 def save_output(df: pd.DataFrame, path: str) -> None:
     if not path.endswith('.csv'):
         path += '.csv'
-    df.to_csv(f'output/{path}', header=True)
+    df.to_csv(f'output/{path}', header=True, index=False)
+
+
+def translate_coords_to_lat(coord: str):
+    return translate_coords(coord, lat=True)
+
+
+def translate_coords_to_lon(coord: str):
+    return translate_coords(coord, lat=False)
+
+
+def load_ports_2() -> pd.DataFrame:
+    with open('data/test_port.csv') as f:
+        df = pd.read_csv(f)
+    return df
+
+
+def load_country() -> pd.DataFrame:
+    with open('data/test_country.csv') as f:
+        df = pd.read_csv(f)
+    return df
+
+
+def load_iso_country() -> pd.DataFrame:
+    with open('data/test_isocountry.csv') as f:
+        df = pd.read_csv(f)
+    return df
+
+
+def translate_coords(coord: str, lat=True) -> str:
+    """
+    Transform unlocode coordinates to lat and lon.
+
+    input format DDDMMP, where DDD is degrees, MM is minutes, and P is the
+    pole.
+    output formatis will be [-]DEG.MIN
+    utput formatis will b
+    """
+    try:
+        coord = coord.split()[0 if lat else 1]
+    except:
+        return ''
+    d, m, pole = coord[:-3], coord[-3:-1], coord[-1]
+    return f'{"-" if pole in ["S", "W"] else ""}{int(d)}.{int(m)}'
 
 
 def main(output: bool) -> None:
     ports = load_ports()
     unlocodes = load_unlocodes()
+    country = load_country()
+    ports2 = load_ports_2()
+    isocountry = load_iso_country()
 
-    merged = pd.merge(
+
+    known_unlocodes = ports2[['code']].copy()
+    known_unlocodes.columns = ['ptrac_unlocode']
+    # attach known ports
+    codes_with_ptrac_ports = pd.merge(
         unlocodes,
-        ports,
-        how='outer',
+        known_unlocodes,
+        how='left',
         left_on='unlocode',
         right_on='ptrac_unlocode'
     )
 
-    # unlocodes found in ptrac that do not exist
-    ptrac_exclusive = (
-        merged.loc[pd.isnull(merged['unlocode'])]['ptrac_unlocode']
+    # attach isocountry for country codes
+    isocountry_codes = isocountry[['alpha_2', 'alpha_3', 'id']].copy()
+    isocountry_codes.columns = ['alpha_2', 'alpha_3', 'iso_country_id']
+    codes_ptrac_and_country = pd.merge(
+        codes_with_ptrac_ports,
+        isocountry_codes,
+        how='left',
+        left_on='lo_country',
+        right_on='alpha_2'
     )
 
-    # unlocodes missing from ptrac
-    missing = merged.loc[pd.isnull(merged['ptrac_unlocode'])]
+    data = codes_ptrac_and_country
 
-    # missing that are nautical PORTS!
-    missing_ports = missing.loc[missing['function'].str.contains('1')]
+    # strip out known unlocodes
+    data = data.loc[pd.isnull(data['ptrac_unlocode'])].copy()
 
-    # proposed attition to ptrac
-    proposed_addition = merged.loc[
-        merged['status'].isin(['AA', 'AC', 'AF', 'AI', 'AI', 'RL', 'QQ']) &
-        pd.isna(merged['ptrac_unlocode'])
+    # generate coordinates
+    data['latitude'] = (
+        np.vectorize(translate_coords_to_lat)(data['coordinates'])
+    )
+    data['longitude'] = (
+        np.vectorize(translate_coords_to_lon)(data['coordinates'])
+    )
+
+    # generate scale
+    data['scale'] = 0.1
+    data['polygon'] = None
+    data['position'] = None
+    data['ihs_port_id'] = None
+    data['world_port_number'] = None
+    data['port_source'] = '2019-1'
+    data['iso_country_id'] = data[['iso_country_id']].fillna('0').astype(int)
+
+    final = data[[
+        'unlocode', 'namewodiacritics', 'alpha_3', 'latitude', 'longitude',
+        'scale', 'polygon', 'position', 'iso_country_id', 'port_source',
+        'ihs_port_id', 'world_port_number'
+    ]].copy()
+    final.columns = [
+        'code', 'name', 'country_code_id', 'latitude', 'longitude',
+        'scale', 'polygon', 'position', 'iso_country_id', 'port_source',
+        'ihs_port_id', 'world_port_number'
     ]
 
     # save csvs
     if output:
         os.makedirs('output', exist_ok=True)
-        save_output(merged, 'full_data')
-        save_output(ptrac_exclusive, 'ptrac_only_ports')
-        save_output(missing, 'missing_unlocodes')
-        save_output(missing_ports, 'missing_unlocodes_that_are_ports')
-        save_output(proposed_addition, 'ports_to_add_to_ptrac')
+        save_output(data, 'raw_data')
+        save_output(final, 'ports_to_insert')
 
     # Launch IPython interactive shell
     user_ns = locals()
